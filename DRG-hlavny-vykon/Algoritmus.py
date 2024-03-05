715d42c9d269c8d5079a7c4b33402d1bb543493e
Oliver Velich <bielovico@gmail.com>
2024-03-05 16:00:30 +0100

    Fixed ERROR message ouptut

--- DRG-hlavny-vykon/Algoritmus.py
+++ DRG-hlavny-vykon/Algoritmus.py
@@ -1,6 +1,6 @@
 def csvWrite(file, line, output):
     if len(output) == 0:
-        output = "ERROR"
+        output = ["ERROR"]
         print("ERROR: Line " + line + " does not have a code.")
     file.write(line + ";" + "~".join(output) + "\n")
 
@@ -26,7 +26,7 @@ def grouperMS(file):
                     mahv = False
 
                 if data[1] == "":
-                    csvWrite(fw, line, "ERROR")
+                    csvWrite(fw, line, ["ERROR"])
                     print("ERROR: Line " + line + " does not have an age.")
                     continue
                 
@@ -508,3 +508,8 @@ def pril16(diags):
 
 print("Na spustenie programu, napiste 'grouperMS(\"nazov_suboru.csv\")' do konzoly.")
 
+import os 
+
+os.chdir('OSN-Algoritmus-MS-2024/DRG-hlavny-vykon')
+
+grouperMS('test_data.csv')
